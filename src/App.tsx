b7265773e905a7c6e7eac4a2b1415517e42d33a3
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-16 18:20:40 +0300

    Initial MVP setup: Digital Tasbih with glass-effect UI

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Tasbih from "./components/Tasbih";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-background">
+      <Tasbih />
+    </div>
+  );
+}
+
+export default App;
